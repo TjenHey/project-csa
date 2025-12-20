acb84e37e17798711f7a9fab9be77abfca415caf
TjenHey <tjen.heylen@gmail.com>
2025-12-20 22:14:48 +0100

    GitHub actions workflow for automatic deployments

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd $HOME
+if [ ! -d "project-csa" ]; then
+    git clone https://github.com/TjenHey/project-csa.git
+    cd project-csa
+else
+    cd project-csa
+    git pull
+    docker compose down
+fi
+
+docker compose up --build -d
